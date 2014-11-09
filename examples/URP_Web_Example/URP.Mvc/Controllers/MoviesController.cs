8341c84578bcda15bae0c0647597b23bcb078696
crann_berry <dan.crann@googlemail.com>
2014-11-09 19:02:06 +0000

    Updates styling and begins to connect the client to server via angularjs.

--- examples/URP_Web_Example/URP.Mvc/Controllers/MoviesController.cs
+++ examples/URP_Web_Example/URP.Mvc/Controllers/MoviesController.cs
@@ -6,22 +6,28 @@ using URP.Domain.Entities;
 
 namespace URP.Mvc.Controllers
 {
+    /// <summary>
+    /// Note: This controller is an example of using the repository and unit of work patterns only.
+    /// This is not production standard code. 
+    /// </summary>
     public class MoviesController : Controller
     {
+        private readonly IUnitOfWork unitOfWork;
         private readonly IRepository<Movie> movieRepository;
 
         /// <summary>
         /// Constructor to initialise the controllers resources.
         /// </summary>
-        public MoviesController(IRepository<Movie> movieRepository)
+        public MoviesController(IUnitOfWork unitOfWork,
+                                IRepository<Movie> movieRepository)
         {
+            this.unitOfWork = unitOfWork;
             this.movieRepository = movieRepository;
         }
 
         /// <summary>
         /// GET: /
         /// </summary>
-        /// <returns>Countries Index View</returns>
         public ActionResult Index()
         {
             try
@@ -31,8 +37,69 @@ namespace URP.Mvc.Controllers
             }
             catch (Exception ex)
             {
+                Response.StatusCode = 500;
                 return View();   
             }
+        }
+
+        /// <summary>
+        /// POST: movies/add
+        /// </summary>
+        public JsonResult Add(Movie movie)
+        {
+            try
+            {
+                movieRepository.Add(movie);
+                unitOfWork.Commit();
+
+                Response.StatusCode = 201;
+                return Json(movie);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+                return new JsonResult();
+            }
+        }
+
+        /// <summary>
+        /// PUT: movies/id
+        /// </summary>
+        public JsonResult Update(Movie movie)
+        {
+            try
+            {
+                movieRepository.Update(movie);
+                unitOfWork.Commit();
+
+                Response.StatusCode = 200;
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+            }
+
+            return new JsonResult();
+        }
+
+        /// <summary>
+        /// DELETE: movies/id
+        /// </summary>
+        public JsonResult Delete(int id)
+        {
+            try
+            {   
+                movieRepository.Delete(movie => movie.MovieId == id);
+                unitOfWork.Commit();
+
+                Response.StatusCode = 200;
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = 500;
+            }
+
+            return new JsonResult();
         }
     }
 }
--- examples/URP_Web_Example/URP.Mvc/Scripts/angular/movies/controllers/index-controller.js
+++ examples/URP_Web_Example/URP.Mvc/Scripts/angular/movies/controllers/index-controller.js
@@ -1,14 +1,38 @@
-﻿urpApp.controller('moviesCtrl',['$scope', function($scope) {
+﻿urpApp.controller('moviesCtrl', ['$scope', '$http', function ($scope, $http) {
 
-    $scope.onAddMovie = function() {
+    $scope.onOpenAddNewMovieModal = function() {
         
     };
 
-    $scope.onSaveNewMovie = function() {
-        
+    $scope.movie = {
+        Title: "test title",
+        TagLine: "test tagline",
+        Length: 130
     };
     
-    $scope.onEditMovie = function () {
+    $scope.onSaveNewMovie = function() {
+        console.log("here");
+
+        var url = 'http://localhost:49388/movies/add';
+
+        $http({
+            method: 'POST',
+            url: url,
+            dataType: 'json',
+            data: angular.toJson($scope.movie),
+            headers: { 'Content-Type': 'application/json' }
+        }).success(function (data, status, headers, config) {
+            $scope.onCloseAddNewMovieModal();
+        }).error(function (data, status, headers, config) {
+            $scope.onCloseAddNewMovieModal();
+        });
+    };
+    
+    $scope.onCloseAddNewMovieModal = function () {
+
+    };
+
+    $scope.onOpenEditMovieModal = function () {
         
     };
     
@@ -16,6 +40,10 @@
         
     };
     
+    $scope.onCloseEditMovieModal = function () {
+
+    };
+
     $scope.onDeleteMovie = function () {
         
     };
