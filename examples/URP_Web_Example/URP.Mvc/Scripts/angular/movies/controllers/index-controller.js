226f2cb04867d14b912dfba48693ea97f46d027a
crann_berry <dan.crann@googlemail.com>
2014-11-11 22:23:32 +0000

    Connects the client-side delete functionality to the server.

--- examples/URP_Web_Example/URP.Mvc/Scripts/angular/movies/controllers/index-controller.js
+++ examples/URP_Web_Example/URP.Mvc/Scripts/angular/movies/controllers/index-controller.js
@@ -1,10 +1,7 @@
 ﻿urpApp.controller('moviesCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.onOpenAddNewMovieModal = function() {
-        // Reset the input fields.
-        $scope.movieEditorData.Title = null;
-        $scope.movieEditorData.TagLine = null;
-        $scope.movieEditorData.Length = 0;
+        $scope.resetEditor();
         
         // Display the dialog.
 
@@ -38,19 +35,69 @@
 
     };
 
-    $scope.onOpenEditMovieModal = function () {
-        
+    $scope.onOpenEditMovieModal = function (movie) {
+        $scope.resetEditor();
+        $scope.loadEditor(movie);
     };
     
     $scope.onSaveUpdatedMovie = function () {
-        
+        $http({
+            method: 'POST',
+            url: 'http://localhost:49388/movies/update',
+            dataType: 'json',
+            data: angular.toJson($scope.movieEditorData),
+            headers: { 'Content-Type': 'application/json' }
+        }).success(function (data, status, headers, config) {
+
+            
+            $scope.onCloseEditMovieModal();
+
+        }).error(function (data, status, headers, config) {
+            // Ideally the api would provide some detailed error info, which can be
+            // formatted and displayed.
+        });
     };
     
     $scope.onCloseEditMovieModal = function () {
 
     };
 
-    $scope.onDeleteMovie = function () {
-        
+    $scope.onDeleteMovie = function (movieId) {
+        $http({
+            method: 'POST',
+            url: 'http://localhost:49388/movies/delete',
+            dataType: 'json',
+            data: { id: movieId },
+            headers: { 'Content-Type': 'application/json' }
+        }).success(function (data, status, headers, config) {
+            
+            var movieToDelete = {};
+            $scope.movies.forEach(function (movie) {
+                if (movie.MovieId === movieId) {
+                    movieToDelete = movie;
+                }
+            });
+
+            if (movieToDelete) {
+                $scope.movies.splice($scope.movies.indexOf(movieToDelete), 1);
+            }
+
+        }).error(function (data, status, headers, config) {
+            // Ideally the api would provide some detailed error info, which can be
+            // formatted and displayed.
+        });
+    };
+
+    $scope.resetEditor = function() {
+        $scope.movieEditorData.MovieId = 0;
+        $scope.movieEditorData.Title = null;
+        $scope.movieEditorData.TagLine = null;
+        $scope.movieEditorData.Length = 0;
+    };
+
+    $scope.loadEditor = function(movie) {
+        $scope.movieEditorData.Title = movie.Title;
+        $scope.movieEditorData.TagLine = movie.TagLine;
+        $scope.movieEditorData.Length = movie.Length;
     };
 }])
